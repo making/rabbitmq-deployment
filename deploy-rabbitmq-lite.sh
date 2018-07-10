f93a07bba8e97c4dd507849a270223c839531d07
Toshiaki Maki <tmaki@pivotal.io>
2018-07-11 00:48:02 +0900

    Add deploy-rabbitmq-lite.sh

---
+++ deploy-rabbitmq-lite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+bosh -d rabbitmq deploy rabbitmq.yml \
+  -o ops-files/rabbitmq-enable-tls.yml \
+  -o ops-files/rabbitmq-add-haproxy.yml \
+  -o ops-files/rabbitmq-add-lb.yml \
+  -l <(cat <<EOF
+haproxy_vm_type: deafault
+rabbitmq_vm_type: default
+rabbitmq_disk_type: "5120"
+tls.common_name: rabbitmq.cf.internal
+tls.alternative_names:
+- "*.sslip.io"
+- "*.ap-northeast-1.elb.amazonaws.com"
+EOF) \
+  --no-redact \
