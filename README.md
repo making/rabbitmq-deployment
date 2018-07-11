ce3f3513fb857e21316751c6dc981bc1fade4a92
Toshiaki Maki <tmaki@pivotal.io>
2018-07-12 01:52:23 +0900

    remove internal security group

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ terraform apply plan
 ```
 LB_DNS_NAME=$(cat terraform.tfstate | jq -r '.modules[0].outputs.rabbitmq_lb_dns_name.value')
 LB_NAME=$(cat terraform.tfstate | jq -r '.modules[0].outputs.rabbitmq_lb_name.value')
-SECURITY_GROUP=$(cat terraform.tfstate | jq -r '.modules[0].outputs.rabbitmq_security_group.value')
 ```
 
 
@@ -24,7 +23,6 @@ SECURITY_GROUP=$(cat terraform.tfstate | jq -r '.modules[0].outputs.rabbitmq_sec
 bosh update-cloud-config <(bosh cloud-config) \
   -o ops-files/cloud-config-add-lb.yml \
   -v rabbitmq_lb_name=${LB_NAME} \
-  -v rabbitmq_security_group=${SECURITY_GROUP} \
 ```
 
 ```
--- terraform/aws/elb.tf
+++ terraform/aws/elb.tf
@@ -18,11 +18,6 @@ variable "vpc_id" {
   type = "string"
 }
 
-variable "vpc_cidr" {
-  type    = "string"
-  default = "10.0.0.0/16"
-}
-
 variable "elb_subnet_ids" {
   type = "list"
 }
@@ -121,16 +116,6 @@ resource "aws_security_group_rule" "outbound" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.rabbitmq.id}"
-}
-
-resource "aws_security_group_rule" "internal" {
-  type        = "ingress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["${var.vpc_cidr}"]
 
   security_group_id = "${aws_security_group.rabbitmq.id}"
 }
